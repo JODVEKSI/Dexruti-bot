e485e0ba0578f6e61ed2a703de5d72bf4feecf2a
JODVEKSI <92377037+JODVEKSI@users.noreply.github.com>
2021-11-19 04:45:21 +1200

    Update main.py

--- main.py
+++ main.py
@@ -69,31 +69,6 @@ async def userinfo(ctx, *, user: discord.Member = None): # b'\xfc'
     embed.set_footer(text='ID: ' + str(user.id))
     return await ctx.send(embed=embed)
 
-@client.command()
-async def serverinfo(ctx):
-  name = str(ctx.guild.name)
-  description = str(ctx.guild.description)
-
-  owner = str(ctx.guild.owner)
-  id = str(ctx.guild.id)
-  region = str(ctx.guild.region)
-  memberCount = str(ctx.guild.member_count)
-
-  icon = str(ctx.guild.icon_url)
-   
-  embed = discord.Embed(
-      title=name + " Server Information",
-      description=description,
-      color=discord.Color.blue()
-    )
-  embed.set_thumbnail(url=icon)
-  embed.add_field(name="Owner", value=owner, inline=True)
-  embed.add_field(name="Server ID", value=id, inline=True)
-  embed.add_field(name="Region", value=region, inline=True)
-  embed.add_field(name="Member Count", value=memberCount, inline=True)
-
-  await ctx.send(embed=embed)
-
 
 
 def get_years(timeBetween, year, reverse):
